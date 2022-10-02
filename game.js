377a2f6652b993c4c96bc405909aa0ca91c58600
willywin99 <willywinsianturi@gmail.com>
2022-10-02 22:28:07 +0700

    feat: game.js Create a New Pattern

--- game.js
+++ game.js
@@ -1,1 +1,12 @@
-alert(`Hello, this is from game.js`);
+let gamePattern = [];
+
+let buttonColours = ['red', 'blue', 'green', 'yellow'];
+
+function nextSequence() {
+  let randomNumber = Math.floor((Math.random() * 3));
+  return randomNumber;
+}
+
+let randomChosenColour = buttonColours[nextSequence()];
+
+gamePattern.push(randomChosenColour);
