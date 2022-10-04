12cd9d4d6cf26bafb049bf9c091236fd12978c74
willywin99 <willywinsianturi@gmail.com>
2022-10-04 21:31:05 +0700

    Refactor Code

--- game.js
+++ game.js
@@ -9,18 +9,22 @@ function nextSequence() {
   gamePattern.push(randomChosenColour);
 
   // 1. Use jQuery to select the button with the same id as the randomChosenColour
-  $(`#${randomChosenColour}`).on('click', function () {
-    // 2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+  // $(`#${randomChosenColour}`).on('click', function () {
+  //   // 2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+  //   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
     
-    // 3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-    let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-    audio.play();
-  });
+  //   // 3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+  //   let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+  //   audio.play();
+  // });
 
-  return randomChosenColour;
+  $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
+  let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+  audio.play();
+
+  // return randomChosenColour;
 }
 
-nextSequence();
+// nextSequence();
 
 
