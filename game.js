33c46b0b45da2a808640f720a86c58ed568d54da
willywin99 <willywinsianturi@gmail.com>
2022-10-04 21:50:30 +0700

    feat: game.js Check Which Button is Pressed

--- game.js
+++ game.js
@@ -2,29 +2,30 @@ let gamePattern = [];
 
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
 
+// 3. At the top of the game.js file, create a new empty array with the name userClickedPattern
+let userClickedPattern = [];
+
+
 function nextSequence() {
   let randomNumber = Math.floor((Math.random() * 4));
   // let randomChosenColour = buttonColours[nextSequence()];
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
-
-  // 1. Use jQuery to select the button with the same id as the randomChosenColour
-  // $(`#${randomChosenColour}`).on('click', function () {
-  //   // 2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
-  //   $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
-    
-  //   // 3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
-  //   let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
-  //   audio.play();
-  // });
-
+  
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
   let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
   audio.play();
-
-  // return randomChosenColour;
+  
+  // 1. Use jQUery to detect when any of the buttons are clicked and trigger a handler function
+  $(`.btn`).on('click', function () {
+    // 2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked
+    let userChosenColour = $(this).attr('id');
+    
+    // 4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
+    userClickedPattern.push(userChosenColour);
+  });
 }
 
-// nextSequence();
+
 
 
