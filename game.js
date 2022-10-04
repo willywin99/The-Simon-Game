c353153041b79dbdfb210d92c6131a54c045ee0a
willywin99 <willywinsianturi@gmail.com>
2022-10-04 21:25:21 +0700

    feat: game.js Show the Sequence to the User with Animations and Sounds

--- game.js
+++ game.js
@@ -7,6 +7,20 @@ function nextSequence() {
   // let randomChosenColour = buttonColours[nextSequence()];
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
+
+  // 1. Use jQuery to select the button with the same id as the randomChosenColour
+  $(`#${randomChosenColour}`).on('click', function () {
+    // 2. Use Google/Stackoverflow to figure out how you can use jQuery to animate a flash to the button selected in step 1.
+    $(`#${randomChosenColour}`).fadeOut(100).fadeIn(100);
+    
+    // 3. Use Google/Stackoverflow to figure out how you can use Javascript to play the sound for the button colour selected in step 1.
+    let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+    audio.play();
+  });
+
+  return randomChosenColour;
 }
+
+nextSequence();
 
 
