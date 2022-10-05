610eb7ecea12ac16c472cf0153363fe0e1befc5b
willywin99 <willywinsianturi@gmail.com>
2022-10-05 22:56:08 +0700

    feat: game.js Add Sounds to Button CLicks

--- game.js
+++ game.js
@@ -2,25 +2,27 @@ let gamePattern = [];
 
 let buttonColours = ['red', 'blue', 'green', 'yellow'];
 
-// 3. At the top of the game.js file, create a new empty array with the name userClickedPattern
 let userClickedPattern = [];
 
-// 1. Use jQUery to detect when any of the buttons are clicked and trigger a handler function
 $(`.btn`).on('click', function () {
-  // 2. Inside the handler, create a new variable called userChosenColour to store the id of the button that got clicked
   let userChosenColour = $(this).attr('id');
   
-  // 4. Add the contents of the variable userChosenColour created in step 2 to the end of this new userClickedPattern
   userClickedPattern.push(userChosenColour);
+
+  playSound(userChosenColour);
 });
 
 function nextSequence() {
   let randomNumber = Math.floor((Math.random() * 4));
-  // let randomChosenColour = buttonColours[nextSequence()];
   let randomChosenColour = buttonColours[randomNumber];
   gamePattern.push(randomChosenColour);
   
   $(`#${randomChosenColour}`).fadeIn(100).fadeOut(100).fadeIn(100);
-  let audio = new Audio(`sounds/${randomChosenColour}.mp3`);
+
+  playSound(randomChosenColour);
+}
+
+function playSound(name) {
+  let audio = new Audio(`sounds/${name}.mp3`);
   audio.play();
 }
